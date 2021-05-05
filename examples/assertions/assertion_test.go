274b6ca8cec8c40fc7f18866a2a91b74dd7c42bc
KYEgo <yegorov.kirill@gmail.com>
2021-05-05 21:39:32 +0500

    docs: add examples

--- examples/assertions/assertion_test.go
+++ examples/assertions/assertion_test.go
@@ -1,1 +1,41 @@
 package assertions
+
+import (
+	"errors"
+	"github.com/kyeeego/cogol"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	cgl := cogol.Init(t)
+
+	g := cgl.Group("Assertions example")
+	{
+		g.T("Successfully adds two numbers", func(c *cogol.Context) {
+			c.Expect(42 + 24).ToBe(66)
+		})
+
+		g.T("True is true, false is false", func(c *cogol.Context) {
+			c.Expect(true).ToBeTrue()
+			c.Expect(false).ToBeFalse()
+		})
+
+		g.T("Success because error is nil", func(c *cogol.Context) {
+			var err error
+			c.Expect(err).ToBeNil()
+		})
+
+		g.T("This one should fail, error is not nil!", func(c *cogol.Context) {
+			err := errors.New("oopsie")
+			c.Expect(err).ToBeNil()
+		})
+
+		g.T("In case we want error to be present, we can use ToBeNotNil method", func(c *cogol.Context) {
+			err := errors.New("oopsie")
+			c.Expect(err).ToBeNotNil()
+		})
+	}
+
+	// Don't forget to run the "Process" method!
+	cgl.Process()
+}
